a39d08525a4fdfc4cc7ad1b6a7af60edcb265e95
Ihar Zharski <ihar.zharski@gmail.com>
2021-04-20 14:21:37 +0200

    fix for changed Youtube page layout. extracting title from metada

--- Obsidian/AddObsidianNote.js
+++ Obsidian/AddObsidianNote.js
@@ -7,13 +7,16 @@
 
 // File manager on iCloud
 var fm = FileManager.iCloud();
-var url = args.shortcutParameter
+var url = args.shortcutParameter;
 
-// Format file content from Shortcuts app
+var youtubeTitleKey = "videoTitle";
+var youtubeRegExp = /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#\&\?]*).*/; //Regex to parse Yotube URLs and extract video id  
+
+//Get file content from Shortcuts app
 var content = await formatDailyNoteContent(url);
 
 //Get the path to the Obsidian file
-var path = formatPath(args.plainTexts[0]);
+var path = formatPath();
 
 createDailyNote(path, content);
 
@@ -22,7 +25,7 @@ Script.complete();
 
 
 function createDailyNote(path, newContent){
-  //If Daily Note file already exists -> ammend conent, otherwise create a new Daily Note file
+  // If Daily Note file already exists -> ammend conent, otherwise create a new Daily Note file
   if (fm.fileExists(path)) { 
     // Get file contents
     var oldContent = fm.readString(path);
@@ -37,47 +40,70 @@ function createDailyNote(path, newContent){
   fm.writeString(path, content);
 }
 
-//Obsidian *.md format for a string with URL: - (title)[url]
+// Obsidian *.md format for a string with URL: - (title)[url]
 async function formatDailyNoteContent(url){
-  //Get page title from URL
+  // Get page title from URL
   let title = await extractTitle(url);
 
-  //Content format is: - (title)[url]
+  // Content format is: - (title)[url]
   return '- [' + title +'](' + url +')';
 }
 
-//Grab page title from HTML source
+// Grab page title from HTML source
 async function extractTitle(url){
-  
-  //Quick workaround to extract video title from Youtube pages via embeded URL: https://www.youtube.com/embed/{video_id}
-  url = youtubeWorkaround(url);
 
-  //Load HTML as a string using Request() https://docs.scriptable.app/request/
-  let req = new Request(url);
-  let res = await req.loadString();
+  var result = "undefined";
 
-  //Grab <title> tag from page HTML source 
-  let titleRegExp = new RegExp("<title[^>]*>([^<]+)<\/title>");
-  let titleMatch = res.match(titleRegExp);
+  // Quick workaround to extract video title from Youtube pages via embeded URL: https://www.youtube.com/embed/{video_id}
+  var match = url.match(youtubeRegExp);
+  if (match && match[7].length == 11){
+    url = 'https://www.youtube.com/embed/' + match[7];
+    
+    console.log(url);
 
-  if (titleMatch)
-    return titleMatch[1];
-  else 
-    return "undefined";
+    result = extractVideoTitleYoutube(url);
+    return result;
+  } 
+  // Otherwise Grab <title> tag from page HTML source 
+  else {
+    // Load HTML as a string using Request() https://docs.scriptable.app/request/
+    let req = new Request(url);
+    let res = await req.loadString();
+
+    // Extract <title> tag from page HTML source 
+    let titleRegExp = new RegExp("<title[^>]*>([^<]+)<\/title>");
+    let titleMatch = res.match(titleRegExp);
+    
+    if (titleMatch)
+      result = titleMatch[1];
+  }
+
+  return result;
 }
 
-//Quick workaround to extract video title from Youtube pages via embeded URL: https://www.youtube.com/embed/{video_id}
-//Some Youtube pages doesn't load video description in a <title> tag
-function youtubeWorkaround(url){
+// Quick workaround to extract video title from Youtube pages via embeded URL: https://www.youtube.com/embed/{video_id}
+// Youtube pages doesn't load video description in a <title> tag
+async function extractVideoTitleYoutube(url){
   
-  //Regex to parse Yotube URLs and extract video id  
-  var youtubeRegExp = /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#\&\?]*).*/;
-  var match = url.match(youtubeRegExp);
+  // Load HTML as a string using Request() https://docs.scriptable.app/request/
+  let req = new Request(url);
+  let res = await req.loadString();
+  
+  // Removing escape characters from HTML/JSON string
+  let normilisedStr = res.replace(/\\/g, '').replace(/"/g, '');
 
-  if ( match && match[7].length == 11 ){
-    url = 'https://www.youtube.com/embed/' + match[7];
+  // Extracting title from embeded into HTML body videoTitle parameter: ...{"videoId":"value","videoShareUrl":"value","videoTitle":"value"}...
+  let startIndex = normilisedStr.indexOf(youtubeTitleKey) + youtubeTitleKey.length + 1; // Add additional character to address colon in json format: "key":"value"
+  let endIndex = startIndex;
+  while(normilisedStr[endIndex] != '}')
+  {
+    endIndex++;
   }
-  return url;
+  let result = normilisedStr.substring(startIndex, endIndex);
+  
+  console.log(result)
+
+  return result;
 } 
 
 //Obsidian format for the new Daily Log file
@@ -85,9 +111,9 @@ function formatNewFile(content){
   return 'Tags: \n\nLinks:\n' + content;
 }
 
-function formatPath(name){
-  // Get the filename type parameter from Shortcuts
-  // This is just yyyy-MM-dd.md by default, can be changed in the Shortcuts app
+function formatPath(){
+  //Get the filename type parameter from Shortcuts  
+  //This is just yyyy-MM-dd.md by default, can be changed in the Shortcuts app
   var filename = args.plainTexts[0] + ".md";
 
   //"daily_notes" is the name of the folder bookmark setting from Scriptable
